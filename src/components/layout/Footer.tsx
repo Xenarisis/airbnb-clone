7b861242f67b2690f1e3e1b0e7fbaa4a1ba3e105
xen <romainrobert.xen@gmail.com>
2026-04-14 16:10:31 +0200

    feature: add footer

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,8 @@
+// components/Footer.tsx
+export default function Footer() {
+    return (
+        <div className="w-full bg-white shadow-md px-8 py-4 flex gap-6">
+        Ceci est une Navbar.
+        </div>
+    )
+}
