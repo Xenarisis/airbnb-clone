617bd1cb2394d03fa2787abad7b17e1549287c82
xen <romainrobert.xen@gmail.com>
2026-04-14 15:32:10 +0200

    feature: add Navbar

---
+++ src/components/layout/Navbar.tsx
@@ -0,0 +1,10 @@
+// components/Navbar.tsx
+import Link from "next/link"
+
+export default function Navbar() {
+  return (
+    <nav className="w-full bg-white shadow-md px-8 py-4 flex gap-6">
+        Ceci est une Navbar.
+    </nav>
+  )
+}
