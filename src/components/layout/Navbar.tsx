f03fb12672e046cb3bbf8c18163665b8e28ce0bc
xen <romainrobert.xen@gmail.com>
2026-04-23 11:33:02 +0200

    add image and make some config

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -1,7 +1,7 @@
 // components/Navbar.tsx
 import Link from "next/link"
 import Image from "next/image"
-import Logo from "./assets/logo.svg"
+import Logo from "@/assets/logo.svg"
 
 export default function Navbar() {
   return (
@@ -9,10 +9,7 @@ export default function Navbar() {
         <div className="bg-gray-900 py-24 sm:py-32">
               <div className="mx-auto max-w-7xl px-6 lg:px-8">
                 <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-5">
-                  <Image
-                    alt="airbnb-pic"
-                    src={Logo}
-                  />
+                  <Logo width={120} height={40} />
               </div>
           </div>
         </div>
