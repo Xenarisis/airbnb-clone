de64b440d38a9d7fec8ee7b859b2888619187663
xen <romainrobert.xen@gmail.com>
2026-04-24 10:14:32 +0200

    feature(Navbar): add element in Navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -5,8 +5,24 @@ import Logo from "@/assets/logo.svg"
 
 export default function Navbar() {
   return (
-    <nav className="w-full bg-white shadow-md px-8 py-4 flex gap-6">
-      <Logo width={120} height={40} />
+    <nav className="w-full bg-mist-200 shadow-md px-8 py-4 flex gap-6">
+      <nav className="flex justify-start">
+        <Logo width={120} height={40} />
+      </nav>
+      <nav className="flex justify-center">
+        recherche
+      </nav>
+      <nav className="flex justify-end">
+        <nav className="mr-2">
+          <p>devenir hote</p>
+        </nav>
+        <nav className="mr-2">
+          <p>language</p>
+        </nav>
+        <nav>
+          <p>burgermenu</p>
+        </nav>
+      </nav>
     </nav>
   )
 }
